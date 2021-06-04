aa188716f63693de7f9b7fa885dba494b6b5d486
doris2018 <lengjd@gmail.com>
2021-06-03 21:08:36 -0700

    add OrderController

--- backEnd/src/main/java/com/example/backEnd/controller/OrderController.java
+++ backEnd/src/main/java/com/example/backEnd/controller/OrderController.java
@@ -1,4 +1,17 @@
 package com.example.backEnd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private DispatchService dispatchService;
+
+    @RequestMapping(value = "/payment", method = RequestMethod.POST)
+    public ResponseEntity<Object> placeOrder(@RequstBody OrderRequestBody orderRequest)
 }
