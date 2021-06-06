0848cb03cba4703e1bb303461423aa5b1774ec4a
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-06-05 18:58:00 -0700

    Update OrderController.java

--- backEnd/src/main/java/com/example/backEnd/controller/OrderController.java
+++ backEnd/src/main/java/com/example/backEnd/controller/OrderController.java
@@ -1,6 +1,10 @@
 package com.example.backEnd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +18,29 @@ public class OrderController {
 
     @RequestMapping(value = "/payment", method = RequestMethod.POST)
     public ResponseEntity<Object> placeOrder(@RequstBody OrderRequestBody orderRequest) {
+
         //request containing the request body
-        OrderResponseBody order Response = new OrderResponseBody();
+        OrderResponseBody orderResponse = new OrderResponseBody();
+        int orderId = orderService.addOrder(orderRequest);
+        orderResponse.setOrderId(orderId);
+
+        //add a dispatched stationId to the response
+        TrackingResponseBody trackingResponseBody = new TrackingResponseBody();
+        int stationId = dispatchService.dispatchStation(orderRequest);
+        trackingResponseBody.setStationid(stationId);
+
+        if (orderResponse == null) {
+            // give a response body class object in the first parameter
+            return new ResponseEntity<>(null, httpStatus.BAD_REQUEST);
+        }
+
+        //return tracking number and status
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
     }
-        
+
+    @RequestMapping(value = "/orders", method = RequestMethod.GET)
+    public ResponseEntity<Object> getCurrentUserOrders() {
+        List<OrderInfoResponseBody> orderList = orderService.getCurrentUserOrders();
+        return new ResponseEntity<>(orderList, HttpStatus.OK);
+    }
 }
