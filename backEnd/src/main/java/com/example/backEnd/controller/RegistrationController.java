02d19211513865da59f129ae9076b46239ecea54
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-06-06 23:47:54 -0700

    Add files via upload

---
+++ backEnd/src/main/java/com/example/backEnd/controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.example.backEnd.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<Object> registerCustomer9@RequestBody
+    RegisterRequestBody request) {
+        //request containing the request body
+        int status = customerService.addCustomer(request);
+        if (status == -1) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+}
