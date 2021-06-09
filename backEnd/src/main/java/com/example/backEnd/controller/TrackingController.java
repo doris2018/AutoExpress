01820ebaed0d79f21b982e5d68e1274c1fd69cad
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-06-08 21:57:26 -0700

    Add files via upload

---
+++ backEnd/src/main/java/com/example/backEnd/controller/TrackingController.java
@@ -0,0 +1,37 @@
+package com.example.backEnd.controller;
+
+import com.example.backEnd.entity.ShippingOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TrackingController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping(value = "/tracking", method = RequestMethod.GET)
+    public ResponseEntity<Object> getShippingOrderById(@RequestParam(value = "orderId") int orderId) {
+        ShippingOrder shippingOrder = orderService.getShippingOrderById(orderId);
+        TrackingResponseBody trackingResponse = new TrackingResponseBody();
+        trackingResponse.setOrderId(orderId);
+        trackingResponse.setSenderAddress(shippingOrder.getSenderAddress());
+        trackingResponse.setReceiverAddress(shippingOrder.getReceiverAddress());
+        trackingResponse.setReceiverName(shippingOrder.getReceiverName());
+        trackingResponse.setCardNumber(shippingOrder.getCardNumber());
+        trackingResponse.setSize(shippingOrder.getSize());
+        trackingResponse.setWeight(shippingOrder.getWeight());
+        trackingResponse.setDescription(shippingOrder.getDescription());
+        trackingResponse.setDeliveryMethod(shippingOrder.getDeliveryMethod());
+        trackingResponse.setFee(shippingOrder.getFee());
+        trackingResponse.setStatus(shippingOrder.getStatus());
+        trackingResponse.setTime(shippingOrder.getTime());
+        trackingResponse.setStationId(shippingOrder.getStation().getStationId());
+        return new ResponseEntity<>(trackingResponse, HttpStatus.OK);
+    }
+}
