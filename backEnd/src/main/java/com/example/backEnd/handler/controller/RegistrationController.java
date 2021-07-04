9eb056fea200373152c7e7a8e7f81a1143dd0d32
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-07-04 12:39:16 -0700

    Update RegistrationController.java

--- backEnd/src/main/java/com/example/backEnd/handler/controller/RegistrationController.java
+++ backEnd/src/main/java/com/example/backEnd/handler/controller/RegistrationController.java
@@ -1,5 +1,4 @@
 package com.example.backEnd.handler.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
