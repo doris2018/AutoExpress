06bad77421e0dfc5cf0299a9357d94c4e8106e43
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-07-12 08:51:58 -0700

    Update Authorities.java

--- backEnd/src/main/java/com/example/backEnd/entity/Authorities.java
+++ backEnd/src/main/java/com/example/backEnd/entity/Authorities.java
@@ -5,7 +5,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 
-
 @Entity
 @Table//(name = "authorities")
 public class Authorities implements Serializable {
