0267c35edd897d4f19af3a4f252b875c266c604d
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-08-09 09:46:23 -0700

    Update OrderService.java

--- backEnd/src/main/java/com/example/backEnd/handler/Service/OrderService.java
+++ backEnd/src/main/java/com/example/backEnd/handler/Service/OrderService.java
@@ -3,7 +3,6 @@ import com.example.backEnd.handler.dao.OrderDao;
 import com.example.backEnd.entity.ShippingOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
 public class OrderService {
 
