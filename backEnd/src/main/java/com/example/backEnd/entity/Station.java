8897e2c335cc3d3479b55dd4b4d6840adc1627fb
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-08-11 09:45:53 -0700

    Update Station.java

--- backEnd/src/main/java/com/example/backEnd/entity/Station.java
+++ backEnd/src/main/java/com/example/backEnd/entity/Station.java
@@ -7,7 +7,6 @@ import java.util.List;
 @Table//(name = "station")
 public class Station implements Serializable {
     private static final long serialVersionUID = 301L;
-
     @Id
     private int stationId;
     private int availableDrones;
